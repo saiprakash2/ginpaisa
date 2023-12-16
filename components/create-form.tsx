1232a251b322b1b1bf68ddf5581dee60a67ceae3
saiprakash <saiprakashshet2@gmail.com>
2023-12-16 16:50:23 +0530

    feat:added validation

--- components/create-form.tsx
+++ components/create-form.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { CustomerField } from '@/lib/definitions';
 import Link from 'next/link';
 import {
@@ -8,10 +9,13 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/components/button';
 import { createExpense } from '@/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function Form() {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createExpense, initialState);
   return (
-    <form action={createExpense}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -31,9 +35,18 @@ export default function Form() {
                 type="number"
                 step="0.01"
                 placeholder="Enter USD amount"
+                aria-describedby="amount-error"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div id="amount-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.amount &&
+                state.errors.amount.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
             </div>
           </div>
         </div>
