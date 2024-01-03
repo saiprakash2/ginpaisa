b756294c056a5c037674db3b799a67519485dd35
saiprakash <saiprakashshet2@gmail.com>
2024-01-04 03:21:08 +0530

    fix: Responsive design fix

--- components/expenseTable.tsx
+++ components/expenseTable.tsx
@@ -16,7 +16,7 @@ export default async function InvoicesTable({
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-          <table className="hidden min-w-full text-gray-900 md:table">
+          <table className="table min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
